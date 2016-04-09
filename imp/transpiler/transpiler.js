1f62806396cd11f6ff3767943faa4c08bb703e8f
Adrian Toncean <adrianton3@gmail.com>
2016-04-09 14:27:57 +0200

    Transpiler takes memory size as an argument

--- imp/transpiler/transpiler.js
+++ imp/transpiler/transpiler.js
@@ -1,41 +1,21 @@
 (function () {
 	'use strict'
 
-	const MEMORY_SIZE = 16
 	const ITERATIONS_MAX = 1000
 
-	const simple = {
-		header: `var m = new Uint8Array(${MEMORY_SIZE});\nvar p = 0;\nvar s = 0;\nvar o = [];`,
-		footer: 'return { output: o, memory: m }',
-		mapping: new Map([
+	function compile (source, { memorySize }) {
+		const header = `var m = new Uint8Array(${memorySize});\nvar p = 0;\nvar s = 0;\nvar o = [];`
+		const footer = 'return { output: o, memory: m }'
+		const mapping = new Map([
 			['+', 'm[p]++;'],
 			['-', 'm[p]--;'],
-			['>', 'p++;'],
-			['<', 'p--;'],
-			['.', 'o.push(m[p]);'],
-			[',', 'm[p] = input[ip]; ip++;'],
-			['[', 'while (m[p]) {'],
-			[']', '}']
-		])
-	}
-
-	const safe = {
-		header: `var m = new Uint8Array(${MEMORY_SIZE});\nvar p = 0;\nvar s = 0;\nvar o = [];`,
-		footer: 'return { output: o, memory: m }',
-		mapping: new Map([
-			['+', 'm[p]++;'],
-			['-', 'm[p]--;'],
-			['>', `p++;\nif (p >= ${MEMORY_SIZE}) { return { output: o, memory: m, error: "pointer out of bounds" }; }`],
+			['>', `p++;\nif (p >= ${memorySize}) { return { output: o, memory: m, error: "pointer out of bounds" }; }`],
 			['<', 'p--;\nif (p < 0) { return { output: o, memory: m, error: "pointer out of bounds" }; }'],
 			['.', 'o.push(m[p]);'],
 			[',', 'm[p] = input[ip]; ip++;'],
 			['[', `while (m[p]) {\ns++;\nif (s > ${ITERATIONS_MAX}) {\nreturn { output: o, memory: m, error: "timeout" };\n}`],
 			[']', '}']
 		])
-	}
-
-	function compile (source) {
-		const { mapping, header, footer } = safe
 
 		const commands = source.split('')
 		const translated = commands.map((char) => mapping.get(char))
